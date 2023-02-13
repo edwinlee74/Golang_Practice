7c74d6b7099dcc16ec6b4007bf94ab5f6574fa35
Shao-Chi <edwinlee1974@gmail.com>
2023-02-13 22:41:19 +0800

    Get Method add

--- rest_client/rest_client.go
+++ rest_client/rest_client.go
@@ -8,14 +8,26 @@ import (
 )
 
 func main() {
-	res, err := http.Get("https://blog.syhlion.tw/sitemap.xml")
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodGet, "https://api.github.com/user/repos", nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
-	defer res.Body.Close()
-	sitemap, err := ioutil.ReadAll(res.Body)
+	req.Header.Add("Accept", "application/vnd.github+json")
+	req.Header.Add("Authorization", "Bearer Your_Token")
+	req.Header.Add("X-GitHub-Api-Version", "2022-11-28")
+
+	response, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
-	fmt.Printf("%s", sitemap)
+	defer response.Body.Close()
+	result, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Printf("%s", result)
 }
